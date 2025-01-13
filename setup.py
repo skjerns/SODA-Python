0689d92867d09322d048e15b0d565bb7b970fe17
skjerns <14980558+skjerns@users.noreply.github.com>
2025-01-13 11:00:06 +0100

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='ws-det',
+      version='0.1',
+      description='Wittkuhn & Schuck (2021) implementation to detect fast sequences in fMRI',
+      long_description_content_type="text/markdown",
+      url='http://github.com/skjerns/WS-det',
+      author='skjerns',
+      author_email='nomail@nomail.com',
+      license='GPLv4',
+      packages=['tdlm'],
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Operating System :: OS Independent",
+      ],)
