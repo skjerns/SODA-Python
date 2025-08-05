d1de8cd9e82bd0bf3a7f3bdac8efef7faa59ea98
skjerns <14980558+skjerns@users.noreply.github.com>
2025-08-05 11:42:52 +0200

    add teste

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# WS-det
+# Slope-Order-Dynamic-Analysis (SODA)
+
 Python implementation of Wittkuhn &amp; Schuck (2021) replay detection method
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name='wsdet',
+setup(name='soda-python',
       version='0.1',
       description='Wittkuhn & Schuck (2021) implementation to detect fast sequences in fMRI',
       long_description_content_type="text/markdown",
-      url='http://github.com/skjerns/WS-det',
+      url='http://github.com/skjerns/SODA-Python',
       author='skjerns',
       author_email='nomail@nomail.com',
       license='GPLv4',
-      packages=['wsdet'],
+      packages=['soda'],
       zip_safe=False,
       classifiers=[
           "Programming Language :: Python :: 3",
